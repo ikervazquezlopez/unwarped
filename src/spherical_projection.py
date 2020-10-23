658213c98b6c8c99e1299cd1181600620e9be7fc
ikervazquezlopez <ikervazquezlopez@u.boisestate.edu>
2020-10-23 16:59:26 -0600

    Project cleaning.

--- src/spherical_projection.py
+++ src/spherical_projection.py
@@ -1,6 +1,13 @@
 import cv2
 import numpy as np
 import math
+import csv
+from os.path import isfile, join, isdir
+import sys
+
+TO_RADIANS = math.pi / 180
+filename_csv = "../data/data.csv"
+
 
 def Rx(p, angle):
     alpha = angle #* (math.pi / 180)
@@ -26,80 +33,68 @@ def Rz(p, angle):
     Z = z
     return (X,Y,Z)
 
-def get_interpolated_color_easy(img, y, x):
-    if x >= img.shape[1]-2 or y >= img.shape[0]-2:
-        return img[int(y),int(x)]
-    else:
-        v0 = img[math.floor(y), math.floor(x)]
-        v1 = img[math.ceil(y), math.ceil(x)]
-
-        color = np.uint8(( v0 + v1) / 2)
-        return color
 
 def get_subpixel(img, y, x):
     patch = cv2.getRectSubPix(img, (1,1), (x,y))
     return patch[0][0]
 
-def get_interpolated_color(img, y, x):
-    dx0 = math.ceil(x) - x
-    dy0 = math.ceil(y) - y
-    x = math.floor(x)
-    y = math.floor(y)
-    if x >= img.shape[1]-2 or y >= img.shape[0]-2:
-        return img[y,x]
-
-    d0 = math.sqrt(dx0*dx0 + dy0*dy0)
-    d1 = 1 - d0
-
-    color = img[y,x] * d0 + img[y+1, x+1] * d1
-
-    return np.uint8(color)
 
 
 
+if __name__ == '__main__':
 
+    filename = sys.argv[1]
+    in_dir = sys.argv[2]
+    out_dir = sys.argv[3]
 
-test_filename = "../data/pano4s.png"
+    rx = sys.argv[4]
+    ry = sys.argv[5]
+    rz = sys.argv[6]
 
-img = cv2.imread(test_filename)
+    img = cv2.imread(join(in_dir, filename))
 
-img_w = img.shape[1]
-img_h = img.shape[0]
+    img_w = img.shape[1]
+    img_h = img.shape[0]
 
-trans = np.zeros_like(img)
+    trans = np.zeros_like(img)
 
-for x in range(0,img_w):
-    for y in range(0,img_h):
-        # Transform x and y to -pi<long<pi and -pi/2<lat<pi/2 respectively
-        xx = 2 * (x + 0.5) / img_w - 1.0;
-        yy = 2 * (y + 0.5) / img_h - 1.0;
-        lng = math.pi * xx;
-        lat = 0.5 * math.pi * yy;
+    for x in range(0,img_w):
+        for y in range(0,img_h):
+            # Transform x and y to -pi<long<pi and -pi/2<lat<pi/2 respectively
+            xx = 2 * (x + 0.5) / img_w - 1.0;
+            yy = 2 * (y + 0.5) / img_h - 1.0;
+            lng = math.pi * xx;
+            lat = 0.5 * math.pi * yy;
 
-        # Compute cartesian coordinates of the sphere surface
-        X = math.cos(lat) * math.cos(lng + math.pi*0.5);
-        Y = math.cos(lat) * math.sin(lng + math.pi*0.5);
-        Z = math.sin(lat);
+            # Compute cartesian coordinates of the sphere surface
+            X = math.cos(lat) * math.cos(lng + math.pi*0.5);
+            Y = math.cos(lat) * math.sin(lng + math.pi*0.5);
+            Z = math.sin(lat);
 
-        # Perform rotations
-        X, Y, Z = Rx((X,Y,Z), math.pi*0.5)
-        #X, Y, Z = Ry((X,Y,Z), math.pi*0.25)
-        #X, Y, Z = Rz((X,Y,Z), math.pi*0.5)
+            # Perform rotations
+            X, Y, Z = Rx((X,Y,Z), float(rx)*TO_RADIANS)
+            #X, Y, Z = Ry((X,Y,Z), float(ry)*TO_RADIANS)
+            X, Y, Z = Rz((X,Y,Z), float(rz)*TO_RADIANS)
 
-        # Compute the latitude and longitude of the rotated points
-        D = math.sqrt(X*X + Y*Y);
-        lat = math.atan2(Z, D);
-        lng = math.atan2(Y, X);
+            # Compute the latitude and longitude of the rotated points
+            D = math.sqrt(X*X + Y*Y);
+            lat = math.atan2(Z, D);
+            lng = math.atan2(Y, X);
 
-        # Transform lat long to plane coordinates
-        ix = (0.5 * lng / math.pi + 0.5) * img_w - 0.5;
-        iy = (lat /math. pi + 0.5) * img_h - 0.5;
+            # Transform lat long to plane coordinates
+            ix = (0.5 * lng / math.pi + 0.5) * img_w - 0.5;
+            iy = (lat /math. pi + 0.5) * img_h - 0.5;
 
-        trans[int(y), int((x + img_w / 4) % img_w)] = get_subpixel(img, iy, ix)
+            trans[int(y), int((x + img_w / 4) % img_w)] = get_subpixel(img, iy, ix)
 
-img = cv2.resize(img, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
-trans = cv2.resize(trans, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
-cv2.imshow('img', img)
-cv2.imshow('trans', trans)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    name = filename.split(".")[0]
+    filename = "{}_{}_{}_{}.png".format(name,rx, ry, rz)
+
+    # Save image
+    cv2.imwrite(join(out_dir, filename), trans)
+
+    # Write data into the csv
+    with open(filename_csv, newline='', mode='a') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow([filename, rx, ry, rz])
+    csvfile.close()
